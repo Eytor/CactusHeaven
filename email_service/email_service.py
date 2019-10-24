173df5f14fd7e74961a73753d8d9604279afaa57
eytor <eythoras16@ru.is>
2019-10-24 13:13:01 +0000

    added email service

--- email_service/email_service.py
+++ email_service/email_service.py
@@ -18,9 +18,9 @@ channel.queue_bind(exchange=exchange_name, queue=email_queue_name, routing_key=c
 
 def send_simple_message(to, subject, body):
     return requests.post(
-        "https://api.mailgun.net/v3/<add-your-domain-here>/messages",
+        "https://api.mailgun.net/v3/sandbox5bd904a0810145a1b558fcbec98c93b0.mailgun.org/messages",
         auth=("api", "<add-your-api-private-key-here>"),
-        data={"from": "Mailgun Sandbox <postmaster@<add-your-domain-here>>",
+        data={"from": "Mailgun Sandbox <postmaster@sandbox5bd904a0810145a1b558fcbec98c93b0.mailgun.org>",
               "to": to,
               "subject": subject,
               "html": body})
